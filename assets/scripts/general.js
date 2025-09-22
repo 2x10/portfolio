5fe031fa45f334f9799623e474ddebd45e6aa3df
poison <arthur.bassgall@protonmail.com>
2025-09-22 22:43:19 +0200

    changes

--- assets/scripts/general.js
+++ assets/scripts/general.js
@@ -1,20 +1,14 @@
-var span = document.getElementById('clockTime');
-var span = document.getElementById('clockDate');
-
-function currentTime() {
-  var time = new Date();
-  var seconds = time.getSeconds();
-  var minutes = time.getMinutes();
-  var hours = time.getHours();
-
-  var day = time.getDay();
-  var month = time.getMonth();
-  var year = time.getFullYear();
-
-
-  //clock.textContent = day + "." + month + "." + year + " - " + ("0" + hours).substr(-2) + ":" + ("0" + minutes).substr(-2) + ":" + ("0" + seconds).substr(-2) ;
-  clockTime.textContent = ("0" + hours).substr(-2) + ":" + ("0" + minutes).substr(-2) + ":" + ("0" + seconds).substr(-2) ;
-  clockDate.textContent = day + "." + month + "." + year;
+function adjustLayout() {
+    const mainDiv = document.querySelector('.main-div');
+    if (window.innerWidth >= 900) 
+    { 
+        mainDiv.style.width = '75%';
+    } 
+    else 
+    {
+        mainDiv.style.width = '100%';
+    }
 }
 
-setInterval(currentTime, 50);    
+window.addEventListener('resize', adjustLayout);
+window.addEventListener('load', adjustLayout);
